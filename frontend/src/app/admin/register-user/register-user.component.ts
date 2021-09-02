9eb862d9a2df05a48712f6a1f4b72ddf8fc26f63
Diego <alejorvela@gmail.com>
2021-09-02 12:07:20 -0500

    first commit

---
+++ frontend/src/app/admin/register-user/register-user.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { RoleService } from '../../services/role.service';
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+@Component({
+  selector: 'app-register-user',
+  templateUrl: './register-user.component.html',
+  styleUrls: ['./register-user.component.css']
+})
+export class RegisterUserComponent implements OnInit {
+
+  registerData: any;
+  message: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number = 2;
+  roles: any;
+
+  constructor(
+    private _userService: UserService,
+    private _roleService: RoleService,
+    private _router: Router,
+    private _snackBar: MatSnackBar
+  ) {
+    this.registerData = {};
+    this.roles = {};
+  }
+
+  ngOnInit(): void {
+    this._roleService.listRole().subscribe(
+      (res) => {
+        console.log(res);
+        this.roles = res;
+      },
+      (err) => {
+        this._router.navigate(['/listUser']);
+        this.message = err.error
+        this.openSnackBarError()
+      }
+    );
+  }
+
+  registerUser() {
+    if (
+      !this.registerData.name ||
+      !this.registerData.email ||
+      !this.registerData.password ||
+      !this.registerData.roleId
+    ) {
+      this.message = 'Failed process: Imcomplete data';
+      this.openSnackBarError();
+      this.registerData = {};
+    } else {
+      this._userService.registerAdmin(this.registerData).subscribe(
+        (res) => {
+          console.log(res);
+          
+          this._router.navigate(['/listUser']);
+          this.message = 'Successfull user registration';
+          this.openSnackBarSuccesfull();
+          this.registerData = {};
+        },
+        (err) => {
+          this.message = err.error;
+          this.openSnackBarError();
+        }
+      );
+    }
+  }
+
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarTrue'],
+    });
+  }
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarFalse'],
+    });
+  }
+
+}
---
+++ frontend/src/app/services/role.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoleService {
+  private env: string;
+
+  constructor(private _http: HttpClient, private _router: Router) {
+    this.env = environment.APP_URL;
+  }
+
+  listRole(name?: string) {
+    return this._http.get<any>(this.env + 'role/listRole');
+  }
+}
